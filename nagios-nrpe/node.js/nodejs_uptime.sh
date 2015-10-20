16be4a600911fc0d239bc3a77839e643f787c1cf
SeanMcGuiganNektan <sean.mcguigan@mfuse.com>
2015-10-20 10:59:44 +0100

    Update nodejs_uptime.sh

--- nagios-nrpe/node.js/nodejs_uptime.sh
+++ nagios-nrpe/node.js/nodejs_uptime.sh
@@ -1,3 +1,11 @@
+# crontab
+# */15 * * * * /bin/touch /tmp/node_timestamp
+# 
+# * * * * * /usr/bin/forever list --plain |\ 
+# awk -F ' '  '{print $3}' |\ 
+# awk -F ':' '{print "DAY",$1,"HOUR",$2,"MIN",$3,"SEC",$4}' |\
+# grep -v processes | tail -1 > /tmp/node_uptime
+
 #!/bin/bash
 
 PID="/var/nektan/app/nektan-lobby/bin/www.pid"
